c6e329f948d6a7355ffdc4e58891d34aec96bb63
Marina <mbakanova2016@yandex.ru>
2021-02-21 17:31:03 +0300

    OnCollisionEnter script

---
+++ Assets/Scripts/EndOfASession.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfASession : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Floor")) {
+            StartCoroutine(OpenInterfaceScreen(3));
+            Debug.Log("Hit");
+        }
+    }
+    IEnumerator OpenInterfaceScreen(float time)
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene("Interface");
+    }
+
+
+}
