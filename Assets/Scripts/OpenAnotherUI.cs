819850903d0c0a3c04278a15071a09adcb9242bd
Marina <mbakanova2016@yandex.ru>
2021-05-26 22:11:14 +0300

    fixed interface script

--- Assets/Scripts/OpenAnotherUI.cs
+++ Assets/Scripts/OpenAnotherUI.cs
@@ -76,58 +76,70 @@ public class OpenAnotherUI : MonoBehaviour
     {
         if (gameObject.name=="ButtonGround")
         {
-            Debug.Log("Klicked ground");
-            ImageFirstRowBack.GetComponent<Image>().color = new Color(0.3764706f, 0.345098f, 0.3372549f, 1f);
-            ImageNature.GetComponent<Image>().sprite = Resources.Load<Sprite>("four-element5") as Sprite;
-            HeaderNow.SetActive(false);
-            FunctionsNow.SetActive(false);
+                Debug.Log("Klicked ground");
+                ImageFirstRowBack.GetComponent<Image>().color = new Color(0.3764706f, 0.345098f, 0.3372549f, 1f);
+                ImageNature.GetComponent<Image>().sprite = Resources.Load<Sprite>("four-element5") as Sprite;
+                HeaderNow.SetActive(false);
+                FunctionsNow.SetActive(false);
 
-            HeaderNext.SetActive(true);
-            FunctionsNext.SetActive(true);
+                HeaderNext.SetActive(true);
+                FunctionsNext.SetActive(true);
 
-            scene_name = "StoneLoc";
-            scene_name_text.text = scene_name;
-            Debug.Log("UITEkst"+ scene_name_text.text);
+                scene_name = "StoneLoc";
+                scene_name_text.text = scene_name;
+                Debug.Log("UITEkst" + scene_name_text.text);
 
         }
         if (gameObject.name == "ButtonWind")
         {
-            Debug.Log("Klicked wind");
-            ImageFirstRowBack.GetComponent<Image>().color = new Color(0.627451f, 0.9254903f, 0.9882354f, 1f);
-            ImageNature.GetComponent<Image>().sprite = Resources.Load<Sprite>("four-element4") as Sprite;
-            HeaderNow.SetActive(false);
-            FunctionsNow.SetActive(false);
+            if (PlayerPrefs.HasKey("Fire"))
+            {
+                Debug.Log("Klicked wind");
+                ImageFirstRowBack.GetComponent<Image>().color = new Color(0.627451f, 0.9254903f, 0.9882354f, 1f);
+                ImageNature.GetComponent<Image>().sprite = Resources.Load<Sprite>("four-element4") as Sprite;
+                HeaderNow.SetActive(false);
+                FunctionsNow.SetActive(false);
 
-            HeaderNext.SetActive(true);
-            FunctionsNext.SetActive(true);
-            scene_name = "WindLoc";
-            scene_name_text.text = scene_name;
+                HeaderNext.SetActive(true);
+                FunctionsNext.SetActive(true);
+                scene_name = "WindLoc";
+                scene_name_text.text = scene_name;
+            }
+            
         }
         if (gameObject.name == "ButtonWater")
         {
-            Debug.Log("Klicked water");
-            ImageFirstRowBack.GetComponent<Image>().color = new Color(0f, 0.7137255f, 0.8705882f, 1f);
-            ImageNature.GetComponent<Image>().sprite = Resources.Load<Sprite>("four-element3") as Sprite;
-            HeaderNow.SetActive(false);
-            FunctionsNow.SetActive(false);
+            if (PlayerPrefs.HasKey("Ground"))
+            {
+                Debug.Log("Klicked water");
+                ImageFirstRowBack.GetComponent<Image>().color = new Color(0f, 0.7137255f, 0.8705882f, 1f);
+                ImageNature.GetComponent<Image>().sprite = Resources.Load<Sprite>("four-element3") as Sprite;
+                HeaderNow.SetActive(false);
+                FunctionsNow.SetActive(false);
 
-            HeaderNext.SetActive(true);
-            FunctionsNext.SetActive(true);
-            scene_name = "WaterLoc";
-            scene_name_text.text = scene_name;
+                HeaderNext.SetActive(true);
+                FunctionsNext.SetActive(true);
+                scene_name = "WaterLoc";
+                scene_name_text.text = scene_name;
+            }
+           
         }
         if (gameObject.name == "ButtonFire")
         {
-            Debug.Log("Klicked fire");
-            ImageFirstRowBack.GetComponent<Image>().color = new Color(0.9333334f, 0.5019608f, 0.3960785f, 1f);
-            ImageNature.GetComponent<Image>().sprite = Resources.Load<Sprite>("four-element2") as Sprite;
-            HeaderNow.SetActive(false);
-            FunctionsNow.SetActive(false);
+            if (PlayerPrefs.HasKey("Water"))
+            {
+                Debug.Log("Klicked fire");
+                ImageFirstRowBack.GetComponent<Image>().color = new Color(0.9333334f, 0.5019608f, 0.3960785f, 1f);
+                ImageNature.GetComponent<Image>().sprite = Resources.Load<Sprite>("four-element2") as Sprite;
+                HeaderNow.SetActive(false);
+                FunctionsNow.SetActive(false);
 
-            HeaderNext.SetActive(true);
-            FunctionsNext.SetActive(true);
-            scene_name = "FireLoc";
-            scene_name_text.text = scene_name;
+                HeaderNext.SetActive(true);
+                FunctionsNext.SetActive(true);
+                scene_name = "FireLoc";
+                scene_name_text.text = scene_name;
+            }
+            
         }
         if (gameObject.name == "ButtonPlay")
         {
