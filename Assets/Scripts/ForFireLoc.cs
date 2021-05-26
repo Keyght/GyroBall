b08a2c2c5f161069e2263abe94e5bd25fd046835
Marina <mbakanova2016@yandex.ru>
2021-05-26 18:31:19 +0300

    check script for prep

--- Assets/Scripts/ForFireLoc.cs
+++ Assets/Scripts/ForFireLoc.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ForFireLoc : MonoBehaviour
 {
+    public GameObject Fire1;
+    public GameObject Fire2;
+    public GameObject Fire3;
+
+    public GameObject Prep1;
+    public GameObject Prep2;
+    public GameObject Prep3;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,41 @@ public class ForFireLoc : MonoBehaviour
     void Update()
     {
         
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("top"))
+        {
+            Fire1.SetActive(true);
+            StartCoroutine(DestroyFirstPrep(3));
+            Debug.Log("top");
+        }
+        else if (collision.gameObject.name.Contains("top001"))
+        {
+            Fire2.SetActive(true);
+            StartCoroutine(DestroySecondPrep(3));
+            Debug.Log("top001");
+        }
+        else if (collision.gameObject.name.Contains("top002"))
+        {
+            Fire3.SetActive(true);
+            StartCoroutine(DestroyThirdPrep(3));
+            Debug.Log("top002");
+        }
+    }
+    IEnumerator DestroyFirstPrep(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Prep1.SetActive(false);
+    }
+    IEnumerator DestroySecondPrep(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Prep2.SetActive(false);
+    }
+    IEnumerator DestroyThirdPrep(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Prep3.SetActive(false);
     }
 }
