34364b60cc4abd672e4e993c05258e1dcc365c92
Marina <mbakanova2016@yandex.ru>
2021-05-26 20:38:13 +0300

    fire ready

--- Assets/Scripts/ForFireLoc.cs
+++ Assets/Scripts/ForFireLoc.cs
@@ -5,57 +5,64 @@ using UnityEngine;
 public class ForFireLoc : MonoBehaviour
 {
     public GameObject Fire1;
-    //public GameObject Fire2;
-    //public GameObject Fire3;
+    public GameObject Fire2;
+    public GameObject Fire3;
+    public GameObject Fire4;
 
     public GameObject Prep1;
-    //public GameObject Prep2;
-    //public GameObject Prep3;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Prep2;
+    public GameObject Prep3;
+    public GameObject Prep4;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name.Contains("top"))
+        if (collision.gameObject.name.Contains("top000"))
         {
             Fire1.SetActive(true);
             StartCoroutine(DestroyFirstPrep(3));
-            Debug.Log("top");
+            Debug.Log("top000");
         }
-        //else if (collision.gameObject.name.Contains("top001"))
-        //{
-        //    Fire2.SetActive(true);
-        //    StartCoroutine(DestroySecondPrep(3));
-        //    Debug.Log("top001");
-        //}
-        //else if (collision.gameObject.name.Contains("top002"))
-        //{
-        //    Fire3.SetActive(true);
-        //    StartCoroutine(DestroyThirdPrep(3));
-        //    Debug.Log("top002");
-        //}
+        else if (collision.gameObject.name.Contains("top001"))
+        {
+            Fire2.SetActive(true);
+            StartCoroutine(DestroySecondPrep(3));
+            Debug.Log("top001");
+        }
+        else if (collision.gameObject.name.Contains("top002"))
+        {
+            Fire3.SetActive(true);
+            StartCoroutine(DestroyThirdPrep(3));
+            Debug.Log("top002");
+        }
+        else if (collision.gameObject.name.Contains("Paper"))
+        {
+            Fire4.SetActive(true);
+            StartCoroutine(DestroyFourthdPrep(3));
+            Debug.Log("Paper");
+        }
     }
     IEnumerator DestroyFirstPrep(float time)
     {
         yield return new WaitForSeconds(time);
-        Prep1.SetActive(false);
+        Destroy(Prep1);
+        Destroy(Fire1);
     }
-    //IEnumerator DestroySecondPrep(float time)
-    //{
-    //    yield return new WaitForSeconds(time);
-    //    Prep2.SetActive(false);
-    //}
-    //IEnumerator DestroyThirdPrep(float time)
-    //{
-    //    yield return new WaitForSeconds(time);
-    //    Prep3.SetActive(false);
-    //}
+    IEnumerator DestroySecondPrep(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(Prep2);
+        Destroy(Fire2);
+    }
+    IEnumerator DestroyThirdPrep(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(Prep3);
+        Destroy(Fire3);
+    }
+    IEnumerator DestroyFourthdPrep(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(Prep4);
+        Destroy(Fire4);
+    }
 }
